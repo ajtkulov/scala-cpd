d29717b01545c584077d5174a2b0f754db83c7d5
Pavel Ajtkulov <pavel.ajtkulov@tinypass.com>
2015-06-08 01:35:49 +0400

    -

--- src/main/scala/cpd/Main.scala
+++ src/main/scala/cpd/Main.scala
@@ -1,9 +1,29 @@
 package cpd
 
+import scalariform.lexer._
+import scalariform.parser._
+
+
 object Main extends App {
-  override def main(args : Array[String]) {
 
+  override def main(args: Array[String]): Unit = {
+    val s =
+      """package some
+        |
+        |import org.scala.test
+        |
+        | // test comment
+        |object X {
+        |  val z: Int = 1
+        |
+        |
+        |}
+      """.stripMargin
+    val res = parseCompilationUnit(s)
+    println(res)
   }
+
+  private def parser(s: String) = new ScalaParser(ScalaLexer.tokenise(s).toArray)
+  private def parseExpression(s: String) = parser(s).expr
+  private def parseCompilationUnit(s: String) = parser(s).compilationUnit
 }
-
-
