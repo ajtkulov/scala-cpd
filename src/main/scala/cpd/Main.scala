78740324c3a94c7c7b5cdc7ccc88912a99470859
Pavel Ajtkulov <ajtkulov@gmail.com>
2015-07-27 03:39:53 +0400

    -

--- src/main/scala/cpd/Main.scala
+++ src/main/scala/cpd/Main.scala
@@ -7,19 +7,35 @@ object Main extends App {
 
   override def main(args: Array[String]): Unit = {
     val tree = parse(FileUtils.fromFileAsString("src/main/resources/1.scala"))
-    traverser.traverse(tree)
+    val tree1 = parse(FileUtils.fromFileAsString("src/main/resources/2.scala"))
 
-    println(traverser.applies.mkString("\n"))
+    val traverser = new Traverse(tree)
+    val traverser1 = new Traverse(tree1)
+
+    val inter = traverser1.vals.toSeq.map(x => x.toString()).intersect(traverser.vals.toSeq.map(x => x.toString()))
+    println(inter.mkString("\n"))
   }
 
-  object traverser extends Traverser {
+  class Traverse(tree: Tree) extends Traverser {
     var applies = List[Apply]()
+    var defs    = List[DefDef]()
+    var vals    = List[ValDef]()
+
+    traverse(tree)
 
     override def traverse(tree: Tree): Unit = tree match {
       case app @ Apply(fun, args) =>
         applies = app :: applies
         super.traverse(fun)
         super.traverseTrees(args)
+      case def1 @ DefDef(mod, term, types, vals, tree1, tree2) =>
+        defs = defs.::(def1)
+        super.traverse(tree1)
+        super.traverse(tree2)
+      case val1 @ ValDef(mod, term, tree1, tree2) =>
+        vals = vals.::(val1)
+        super.traverse(tree1)
+        super.traverse(tree2)
 
       case _ => super.traverse(tree)
     }
