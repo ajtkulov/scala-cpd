e2fe08ec931c0bbfd114c0af46ec9f86c0dca80f
Pavel Ajtkulov <ajtkulov@gmail.com>
2016-06-22 18:34:10 +0400

    -

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,18 +1,11 @@
 logLevel := Level.Warn
 
-scalaVersion := "2.10.4"
+addSbtPlugin("com.eed3si9n" %% "sbt-assembly" % "0.10.2")
 
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.2")
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
+addSbtPlugin("com.github.mpeltonen" %% "sbt-idea" % "1.6.0")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.7.0")
 
-addSbtPlugin("com.gu" % "sbt-teamcity-test-reporting-plugin" % "1.5")
-
-
-//addSbtPlugin("com.github.ajtkulov" % "scala-cpd" % "0.2")
-
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.5.0")
 
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0") 
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
--- src/main/scala/cpd/Cache.scala
+++ src/main/scala/cpd/Cache.scala
@@ -6,6 +6,10 @@ import scala.meta.tokens.Token.Space
 
 
 object Some1 {
+  def normalize(tree: Tree): String = {
+    tree.tokens.filterNot(x => x.is[Token.Space] || x.is[Token.Tab]).mkString("")
+  }
+
   val z1: Parsed[Source] =
     """ class some1(value: Int) {
           for (i <- 1 to 10) yield     { println(123)
@@ -24,19 +28,20 @@ object Some1 {
          }
     """.parse[Source]
 
+
+  //  z1.get.traverse()
+
   z1.get.stats.head.traverse {
     //      case a: scala.meta.Decl => println(1235345)
     //      case a: scala.meta.Defn.Class => println(a); println("class")
     case a: scala.meta.Term.For => println(a); println("*for")
     case a: scala.meta.Term.ForYield => println(a); println("*for yield")
-    case a: scala.meta.Defn.Class => println(a); println("*class"); println(a.tokens.filterNot(x => x.is[Token.Space] || x.is[Token.Tab]) mkString(", "));
+    case a: scala.meta.Defn.Class => println(a); println("*class"); println(normalize(a));
     case a: scala.meta.Defn.Val => println(a); println("*val")
     case a: scala.meta.Defn.Def => println(a); println("*def")
     case a: scala.meta.Defn => println(a); println("*defn")
-    case a: scala.meta.Stat if (a.children.length > 1) => println(a); println(s"*statsssss: ${a.children.length}");  println(a.children.mkString("[", ", ", "]"))
+    case a: scala.meta.Stat if (a.children.length > 1) => println(a); println(s"*statsssss: ${a.children.length}"); println(a.children.mkString("[", ", ", "]"))
     case a: scala.meta.Stat => println(a); println("*stat");
-
-    //      case a: Tree => println(a)
     case _ =>
   }
 }
