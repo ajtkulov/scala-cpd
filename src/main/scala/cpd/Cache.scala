48ffa2d64e84674c73d4695085e59b7d33fd9f04
Pavel Ajtkulov <ajtkulov@gmail.com>
2016-07-13 02:55:56 +0300

    -

--- src/main/scala/cpd/Cache.scala
+++ src/main/scala/cpd/Cache.scala
@@ -1,9 +1,44 @@
 package cpd
 
+import cpd.Some1._
+
+import scala.collection.immutable.Seq
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.meta.Term.Block
 import scala.meta._
 
+case class SourceFile(path: Path) {
+  lazy val source: Source = Some1.parse(FileUtils.readFile(path))
+  lazy val subTrees = Some1.traverse(source)
+  lazy val strings: List[(NormalizedSource, Tree)] = subTrees.map(x => (Some1.normalize(x), x))
+}
+
+case class ResultItemPart(source: Path, tree: Tree) {}
+
+case class ResultItem(fst: ResultItemPart, snd: ResultItemPart, source: NormalizedSource) {}
+
+case class Project(files: List[Path]) {
+  def handle(): List[ResultItem] = {
+
+    val res = ArrayBuffer[ResultItem]()
+    val global: mutable.Map[String, (Tree, Path)] = scala.collection.mutable.Map[String, (Tree, Path)]()
+    for (path <- files; item <- SourceFile(path).strings) {
+      val occ: Option[(Tree, Path)] = global.put(item._1, (item._2, path))
+      if (occ.isDefined) {
+        val prev: (Tree, Path) = occ.get
+        res.append(ResultItem(ResultItemPart(path, item._2), ResultItemPart(prev._2, item._2), item._1))
+      }
+    }
+
+    res.toList
+  }
+}
 
 object Some1 {
+  type Path = String
+  type NormalizedSource = String
+  lazy val minSize: Int = 10
   def normalize(tree: Tree): String = {
     tree.tokens.filterNot(x => x.is[Token.Space] || x.is[Token.Tab] || x.is[Token.CR] || x.is[Token.LF]).mkString("")
   }
@@ -20,20 +55,40 @@ object Some1 {
     source.parse[Stat].get
   }
 
-  def traverse(source: Source): Unit = {
-    source.stats.foreach(stat =>
-      stat.traverse {
-        case a: scala.meta.Term.For => println(a); println("*for")
-        case a: scala.meta.Term.ForYield => println(a); println("*for yield")
-        case a: scala.meta.Defn.Class => println(a); println("*class"); println(normalize(a));
-        case a: scala.meta.Defn.Val => println(a); println("*val")
-        case a: scala.meta.Defn.Def => println(a); println("*def")
-        case a: scala.meta.Defn => println(a); println("*defn")
-        case a: scala.meta.Stat if (a.children.length > 1) => println(a); println(s"*statsssss: ${a.children.length}"); println(a.children.mkString("[", ", ", "]"))
-        case a: scala.meta.Stat => println(a); println("*stat");
-        case _ =>
-      })
+  def traverse(source: Source): List[Tree] = {
+    val buffer = ArrayBuffer[Tree]()
 
+    def addTree(tree: Tree): Unit = {
+      if (size(tree) > minSize) {buffer.append(tree) }
+    }
+
+    source.stats.foreach(stat =>
+      if (size(stat) > minSize) {
+        stat.traverse {
+          case a: scala.meta.Term.Block => {
+            addTree(a)
+            val stats = a.stats
+
+            for (i <- 0 until stats.size - 1; j <- i + 1 until stats.size) {
+              val sub: Seq[Stat] = stats.slice(i, j)
+              addTree(Block(sub))
+            }
+          }
+          case a: scala.meta.Term.For => addTree(a)
+          case a: scala.meta.Term.ForYield => addTree(a)
+          case a: scala.meta.Defn.Class => addTree(a)
+          case a: scala.meta.Defn.Val => addTree(a)
+          case a: scala.meta.Defn.Def => addTree(a)
+          case a: scala.meta.Defn => addTree(a)
+          case a: scala.meta.Stat => addTree(a)
+          case _ =>
+        }
+      })
+    buffer.toList
+  }
+
+  def size(values: Seq[Tree]): Int = {
+    values.map(x => size(x)).sum
   }
 
   def size(stat: Tree): Int = {
@@ -51,6 +106,4 @@ object Some1 {
 
     res
   }
-
-
 }
